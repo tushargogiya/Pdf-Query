4b3315ca50e4594b535de2fd374af3ec992de2fa
tushargogiya <tushargogiya97@gmail.com>
2026-03-14 13:14:11 +0530

    Fix: use langchain_text_splitters for modern LangChain compatibility

--- pdf_backend.py
+++ pdf_backend.py
@@ -1,7 +1,7 @@
 import os
 import tempfile
 from langchain_community.document_loaders import PDFPlumberLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import AzureChatOpenAI
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
